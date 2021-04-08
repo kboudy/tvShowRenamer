70e207267d441dda093e725802e6d6e1e88c93f8
Keith Boudreau <kboudy@gmail.com>
2021-04-08 18:41:29 -0400

    npm i -g, tv-show-renamer

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 ## This script:
-* presumes you're in the show directory, named accordingly
-* looks for any files with SxxExx
-* deletes any non-SxxExx files
-* choose the biggest of each, deletes the rest
-* renames the files
-* removes any empty subdirectories
 
-## tldr:
-* run `node ~/keith_apps/tvShowRenamer` from the show dir
+- presumes you're in the show directory, named accordingly
+- looks for any files with SxxExx
+- deletes any non-SxxExx files
+- choose the biggest of each, deletes the rest
+- renames the files
+- removes any empty subdirectories
+
+* installation _(from this directory, after cloning repo)_:
+  - `npm i -g .`
+
+- run `tv-show-renamer` from the show dir
--- index.js
+++ index.js
@@ -5,96 +5,102 @@ const _ = require('lodash'),
   chalk = require('chalk'),
   prompt = require('prompt-sync')({ sigint: true });
 
-let showDir = process.cwd();
-let showName = require('path').basename(showDir);
+const organizeTVDir = () => {
+  let showDir = process.cwd();
+  let showName = require('path').basename(showDir);
 
-console.log();
-console.log(
-  chalk.red(
-    `*******************************************************************************************************`
-  )
-);
-
-const response = prompt(
-  chalk.red(
-    `Please confirm that you want to reorganize (rename & delete) this directory's tv files by typing "yes":`
-  )
-);
-if (response !== 'yes') {
-  console.log(chalk.yellow(`process aborted`));
   console.log();
-  process.exit(1);
-}
-
-let seFiles = {};
-
-let walker = walk.walk(showDir, { followLinks: false });
-
-walker.on('file', function (root, stat, next) {
-  let matches = stat.name.match(/S\d\d[ ]?E\d\d/gi, '');
-  if (matches && matches.length > 0) {
-    let seasonEpisode = matches[0].toUpperCase().split(' ').join('');
-    if (!(seasonEpisode in seFiles)) {
-      seFiles[seasonEpisode] = [];
-    }
-    let file = path.join(root, stat.name);
-
-    let fInfo = {};
-    fInfo.path = file;
-    fInfo.size = fs.statSync(file).size;
-    seFiles[seasonEpisode].push(fInfo);
-  } else {
-    let fileToDelete = path.join(root, stat.name);
+  console.log(
     chalk.red(
-      console.log(
-        `- deleting ${fileToDelete} (${fs.statSync(fileToDelete).size})`
-      )
-    );
-    fs.unlinkSync(fileToDelete);
-  }
-  next();
-});
+      `*******************************************************************************************************`
+    )
+  );
 
-function getAllSubDirs(dir) {
-  var results = [];
-  var list = fs.readdirSync(dir);
-  list.forEach(function (file) {
-    file = dir + '/' + file;
-    var stat = fs.statSync(file);
-    if (stat && stat.isDirectory()) {
-      results.push(file);
-      /* Recurse into a subdirectory */
-      results = results.concat(getAllSubDirs(file));
+  const response = prompt(
+    chalk.red(
+      `Please confirm that you want to reorganize (rename & delete) this directory's tv files by typing "yes":`
+    )
+  );
+  if (response !== 'yes') {
+    console.log(chalk.yellow(`process aborted`));
+    console.log();
+    process.exit(1);
+  }
+
+  let seFiles = {};
+
+  let walker = walk.walk(showDir, { followLinks: false });
+
+  walker.on('file', function (root, stat, next) {
+    let matches = stat.name.match(/S\d\d[ ]?E\d\d/gi, '');
+    if (matches && matches.length > 0) {
+      let seasonEpisode = matches[0].toUpperCase().split(' ').join('');
+      if (!(seasonEpisode in seFiles)) {
+        seFiles[seasonEpisode] = [];
+      }
+      let file = path.join(root, stat.name);
+
+      let fInfo = {};
+      fInfo.path = file;
+      fInfo.size = fs.statSync(file).size;
+      seFiles[seasonEpisode].push(fInfo);
+    } else {
+      let fileToDelete = path.join(root, stat.name);
+      chalk.red(
+        console.log(
+          `- deleting ${fileToDelete} (${fs.statSync(fileToDelete).size})`
+        )
+      );
+      fs.unlinkSync(fileToDelete);
+    }
+    next();
+  });
+
+  function getAllSubDirs(dir) {
+    var results = [];
+    var list = fs.readdirSync(dir);
+    list.forEach(function (file) {
+      file = dir + '/' + file;
+      var stat = fs.statSync(file);
+      if (stat && stat.isDirectory()) {
+        results.push(file);
+        /* Recurse into a subdirectory */
+        results = results.concat(getAllSubDirs(file));
+      }
+    });
+    return results;
+  }
+
+  walker.on('end', function () {
+    for (var se in seFiles) {
+      let files = _.sortBy(seFiles[se], 'size');
+      let primaryFile = files[files.length - 1];
+      for (i = 0; i < files.length - 1; i++) {
+        chalk.red(
+          console.log(`- deleting ${files[i].path} (${files[i].size})`)
+        );
+        fs.unlinkSync(files[i].path);
+      }
+
+      let expectedLocation = path.join(
+        showDir,
+        `${showName} ${se}${path.extname(primaryFile.path)}`
+      );
+      if (!fs.existsSync(expectedLocation)) {
+        console.log(
+          chalk.yellow(`- moving ${primaryFile.path} to ${expectedLocation}`)
+        );
+        fs.renameSync(primaryFile.path, expectedLocation);
+      }
+    }
+
+    let subDirs = _.sortBy(getAllSubDirs(showDir), (d) => d.length);
+    subDirs = subDirs.reverse();
+    for (i = 0; i < subDirs.length; i++) {
+      chalk.gray(console.log(`- removing empty dir: ${subDirs[i]}`));
+      fs.rmdirSync(subDirs[i]);
     }
   });
-  return results;
-}
+};
 
-walker.on('end', function () {
-  for (var se in seFiles) {
-    let files = _.sortBy(seFiles[se], 'size');
-    let primaryFile = files[files.length - 1];
-    for (i = 0; i < files.length - 1; i++) {
-      chalk.red(console.log(`- deleting ${files[i].path} (${files[i].size})`));
-      fs.unlinkSync(files[i].path);
-    }
-
-    let expectedLocation = path.join(
-      showDir,
-      `${showName} ${se}${path.extname(primaryFile.path)}`
-    );
-    if (!fs.existsSync(expectedLocation)) {
-      console.log(
-        chalk.yellow(`- moving ${primaryFile.path} to ${expectedLocation}`)
-      );
-      fs.renameSync(primaryFile.path, expectedLocation);
-    }
-  }
-
-  let subDirs = _.sortBy(getAllSubDirs(showDir), (d) => d.length);
-  subDirs = subDirs.reverse();
-  for (i = 0; i < subDirs.length; i++) {
-    chalk.gray(console.log(`- removing empty dir: ${subDirs[i]}`));
-    fs.rmdirSync(subDirs[i]);
-  }
-});
+module.exports = organizeTVDir;
